55b7761d39a5595e164067c355089ae16391b60c
Paweł Turoń <34440020+pturo@users.noreply.github.com>
2020-04-02 19:06:47 +0200

    Update #1
    Add project files

---
+++ Assets/Scripts/CrystalPicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CrystalPicker : MonoBehaviour
+{
+    public Text countText;
+    public GameObject particles;
+
+    void Start()
+    {
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Crystal"))
+        {
+            Instantiate(particles, transform.position, Quaternion.identity);
+            other.gameObject.SetActive(false);
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        GameObject[] crystals = GameObject.FindGameObjectsWithTag("Crystal");
+        countText = GameObject.Find("CrystalsText").GetComponent<Text>();
+        countText.text = "Kryształów: " + crystals.Length;
+        if (crystals.Length == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Cursor.visible = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    float speed = 500f;     // Ball's speed.
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+    }
+
+    void Update() { }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Fix the rotation to match the direction of the camera's y axis.
+        Vector3 fixedMovement = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movement;
+
+        rb.AddForce(fixedMovement * speed * Time.deltaTime);
+    }
+}
